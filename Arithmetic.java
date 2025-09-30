9c92b1f6d83a533799ee44a0475dbb39a1c0c1a6
Vidhya <143831108+VidhyaPonnusamy-gce@users.noreply.github.com>
2025-09-30 18:51:24 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,13 @@
+class Arithmetic{
+public static void main(String args[]){
+	int n=5;
+	double a=1.5,sum=0.0;
+	int d=3;
+	for(int i=0;i<n;i++){
+		sum+=a;
+		a+=d;
+	
+	}
+	System.out.println("ap" + sum);
+}
+}
