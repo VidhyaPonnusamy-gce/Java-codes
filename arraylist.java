6dd91b9edfc74e6d9a80c1a195212f64c577b20e
Vidhya <143831108+VidhyaPonnusamy-gce@users.noreply.github.com>
2025-09-30 18:51:52 +0530

    Add files via upload

---
+++ arraylist.java
@@ -0,0 +1,12 @@
+public class arraylist{
+	public static void main(String args[]){
+	String s1="This is a java.";
+	
+	int l=s1.length();
+	String[] w=s1.split(" ");
+	for(int i=0;i<w.length;i++){
+		System.out.println(w[i]+"-"+i);
+	}
+}
+}
+		//wrong program
