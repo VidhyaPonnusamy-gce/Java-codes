fb809ade9877b9c43eb5fc40a1e1c4a98ab5c573
Vidhya <143831108+VidhyaPonnusamy-gce@users.noreply.github.com>
2025-09-30 18:49:20 +0530

    Add files via upload

---
+++ Addcomplexnumber.java
@@ -0,0 +1,15 @@
+public class Addcomplexnumber{
+    public static void main(String args[]){
+        int real1=3;
+        int imaginary1=5;
+        int real2=4;
+        int imaginary2=4;
+        int result=real1+real2;
+        int result1=imaginary1+imaginary2;
+        System.out.print(result);
+        System.out.print("+");
+        System.out.print(result1);
+        System.out.println("i");
+        
+    }
+}
